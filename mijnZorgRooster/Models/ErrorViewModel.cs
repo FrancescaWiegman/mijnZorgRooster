041b10a3a0b1417bc75061f290c791ee1f51caa1
Francesca <francescawiegman@ziggo.nl>
2018-12-18 19:21:14 +0100

    Add project files.

---
+++ mijnZorgRooster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mijnZorgRooster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
