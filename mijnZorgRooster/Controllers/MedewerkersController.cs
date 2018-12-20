01fe964487d1f6f6bfd16ebc2dc4a4b3a9dd9cb4
Francesca Wiegman <F.Wiegman@student.han.nl>
2018-12-20 14:27:32 +0100

    ...

---
+++ mijnZorgRooster/Controllers/MedewerkersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mijnZorgRooster.Data;
+using mijnZorgRooster.Models;
+
+namespace mijnZorgRooster.Controllers
+{
+    public class MedewerkersController : Controller
+    {
+        private readonly ZorginstellingContext _context;
+
+        public MedewerkersController(ZorginstellingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Medewerkers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Medewerkers.ToListAsync());
+        }
+
+        // GET: Medewerkers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medewerker = await _context.Medewerkers
+                .FirstOrDefaultAsync(m => m.medewerkerID == id);
+            if (medewerker == null)
+            {
+                return NotFound();
+            }
+
+            return View(medewerker);
+        }
+
+        // GET: Medewerkers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medewerkers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("medewerkerID,Voornaam,Achternaam,Tussenvoegsels,Telefoonnummer,MobielNummer,Emailadres,Adres,Postcode,Woonplaats,Geboortedatum")] Medewerker medewerker)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(medewerker);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(medewerker);
+        }
+
+        // GET: Medewerkers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medewerker = await _context.Medewerkers.FindAsync(id);
+            if (medewerker == null)
+            {
+                return NotFound();
+            }
+            return View(medewerker);
+        }
+
+        // POST: Medewerkers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("medewerkerID,Voornaam,Achternaam,Tussenvoegsels,Telefoonnummer,MobielNummer,Emailadres,Adres,Postcode,Woonplaats,Geboortedatum")] Medewerker medewerker)
+        {
+            if (id != medewerker.medewerkerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(medewerker);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedewerkerExists(medewerker.medewerkerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(medewerker);
+        }
+
+        // GET: Medewerkers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medewerker = await _context.Medewerkers
+                .FirstOrDefaultAsync(m => m.medewerkerID == id);
+            if (medewerker == null)
+            {
+                return NotFound();
+            }
+
+            return View(medewerker);
+        }
+
+        // POST: Medewerkers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var medewerker = await _context.Medewerkers.FindAsync(id);
+            _context.Medewerkers.Remove(medewerker);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedewerkerExists(int id)
+        {
+            return _context.Medewerkers.Any(e => e.medewerkerID == id);
+        }
+    }
+}
---
+++ mijnZorgRooster/Migrations/20181220132229_zorgapp.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mijnZorgRooster.Migrations
+{
+    public partial class zorgapp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medewerker",
+                columns: table => new
+                {
+                    medewerkerID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Voornaam = table.Column<string>(nullable: true),
+                    Achternaam = table.Column<string>(nullable: true),
+                    Tussenvoegsels = table.Column<string>(nullable: true),
+                    Telefoonnummer = table.Column<string>(nullable: true),
+                    MobielNummer = table.Column<string>(nullable: true),
+                    Emailadres = table.Column<string>(nullable: true),
+                    Adres = table.Column<string>(nullable: true),
+                    Postcode = table.Column<string>(nullable: true),
+                    Woonplaats = table.Column<string>(nullable: true),
+                    Geboortedatum = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medewerker", x => x.medewerkerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Certificaat",
+                columns: table => new
+                {
+                    CertificaatID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MedewerkerID = table.Column<int>(nullable: false),
+                    Datum = table.Column<DateTime>(nullable: false),
+                    geldigTot = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Certificaat", x => x.CertificaatID);
+                    table.ForeignKey(
+                        name: "FK_Certificaat_Medewerker_MedewerkerID",
+                        column: x => x.MedewerkerID,
+                        principalTable: "Medewerker",
+                        principalColumn: "medewerkerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contract",
+                columns: table => new
+                {
+                    ContractID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BeginDatum = table.Column<DateTime>(nullable: false),
+                    Einddatum = table.Column<DateTime>(nullable: false),
+                    ContractUren = table.Column<int>(nullable: false),
+                    medewerkerID = table.Column<int>(nullable: true),
+                    verlofDagenPerJaar = table.Column<int>(nullable: false),
+                    ParttimePercentage = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contract", x => x.ContractID);
+                    table.ForeignKey(
+                        name: "FK_Contract_Medewerker_medewerkerID",
+                        column: x => x.medewerkerID,
+                        principalTable: "Medewerker",
+                        principalColumn: "medewerkerID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rol",
+                columns: table => new
+                {
+                    RolID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    medewerkerID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rol", x => x.RolID);
+                    table.ForeignKey(
+                        name: "FK_Rol_Medewerker_medewerkerID",
+                        column: x => x.medewerkerID,
+                        principalTable: "Medewerker",
+                        principalColumn: "medewerkerID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Certificaat_MedewerkerID",
+                table: "Certificaat",
+                column: "MedewerkerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contract_medewerkerID",
+                table: "Contract",
+                column: "medewerkerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rol_medewerkerID",
+                table: "Rol",
+                column: "medewerkerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Certificaat");
+
+            migrationBuilder.DropTable(
+                name: "Contract");
+
+            migrationBuilder.DropTable(
+                name: "Rol");
+
+            migrationBuilder.DropTable(
+                name: "Medewerker");
+        }
+    }
+}
