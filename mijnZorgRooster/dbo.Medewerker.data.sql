5cc484618d8160e76ff87c9df4fe3ff00c84bff8
Francesca <francescawiegman@ziggo.nl>
2019-01-22 18:26:54 +0100

    sql

---
+++ mijnZorgRooster/dbo.Medewerker.data.sql
@@ -0,0 +1,23 @@
+﻿SET IDENTITY_INSERT [dbo].[Medewerker] ON
+INSERT INTO [dbo].[Medewerker] ([MedewerkerID], [Voornaam], [Achternaam], [Tussenvoegsels], [Telefoonnummer], [MobielNummer], [Emailadres], [Adres], [Postcode], [Woonplaats], [Geboortedatum], [leeftijdInJaren]) VALUES 
+(1,'Maris','Chapman','tempor','1-4983-458734','1-66-79356597','varius.Nam.porttitor@aliquameuaccumsan.com','4882 Quis Ave','0013 GH','Caramanico Terme','1969-12-31 16:00:00',86),
+(2,'Ignatius','Bridges','Sed','1-9543-764867','1-39-57919354','tortor@laoreet.edu','Ap #763-8706 Urna. Rd.','6462 EC','Casoli','1969-12-31 16:00:00',58),
+(3,'Emerald','Carson','eget','1-9533-599598','1-85-15413865','massa@aliquetnecimperdiet.net','P.O. Box 199, 7211 Donec Av.','9518 TJ','Holyhead','1969-12-31 16:00:00',87),
+(4,'Jolene','Dawson','viverra.','1-1971-126831','1-13-71761399','aliquet.magna@metusurna.com','Ap #464-3164 Varius Street','3742 JH','Pulle','1969-12-31 16:00:00',32),
+(5,'Mikayla','Webster','vitae','1-5283-476282','1-46-82671315','pede.nec.ante@velit.ca','1560 Sed St.','2019 OJ','Roksem','1969-12-31 16:00:00',31),
+(6,'Vanna','Hobbs','consequat','1-6523-113634','1-51-79513848','at.auctor@tinciduntadipiscingMauris.edu','P.O. Box 305, 9526 Dictum Rd.','5245 UY','Penrith','1969-12-31 16:00:00',93),
+(7,'Kenyon','Morris','diam','1-3625-727128','1-19-58344431','tortor.dictum.eu@disparturientmontes.co.uk','214-8127 Enim St.','9410 LJ','Grand-Hallet','1969-12-31 16:00:00',82),
+(8,'Cameran','Bird','ultrices','1-7336-278716','1-41-62261577','tortor.dictum@ultrices.com','P.O. Box 866, 5421 Ridiculus Av.','2245 BD','Grafton','1969-12-31 16:00:00',94),
+(9,'Giacomo','Morales','Aenean','1-2652-638895','1-66-94471532','Fusce@Quisqueornare.edu','Ap #210-1208 Auctor Ave','8936 ZC','Erchie','1969-12-31 16:00:00',89),
+(10,'Jasper','Deleon','dui,','1-5822-673695','1-27-22141538','magna.tellus.faucibus@egestas.org','987-9110 Nullam St.','9553 CL','Palmerston North','1969-12-31 16:00:00',56),
+(11,'Dale','Vaughan','blandit.','1-4651-741695','1-84-55668311','ipsum.ac@nonummy.com','6193 Aliquam Avenue','8841 RT','Calera','1969-12-31 16:00:00',95),
+(12,'Rosalyn','Cooke','nibh','1-6388-175824','1-84-96625146','Ut@Class.edu','Ap #333-7820 Dignissim Ave','6218 DD','Lauder','1969-12-31 16:00:00',48),
+(13,'Giacomo','Burgess','Maecenas','1-5612-159329','1-48-11792945','Ut.tincidunt@maurisIntegersem.com','445-2322 Vel Av.','8571 HC','Alkmaar','1969-12-31 16:00:00',78),
+(14,'Sybill','Munoz','luctus','1-9491-122434','1-57-93984857','urna.suscipit@faucibusorci.co.uk','9207 Nonummy Avenue','9657 PH','Solihull','1969-12-31 16:00:00',81),
+(15,'Ryan','Knight','sagittis','1-1229-321529','1-55-71331587','laoreet.posuere@aliquam.org','2036 Accumsan Ave','2342 NP','Pointe-aux-Trembles','1969-12-31 16:00:00',84),
+(16,'Hilary','Hammond','montes,','1-7677-762455','1-36-49213446','id.risus.quis@utnullaCras.edu','P.O. Box 256, 3864 Orci Road','1545 TW','Cassano Spinola','1969-12-31 16:00:00',70),
+(17,'Callie','Terrell','egestas','1-4681-484619','1-13-86442685','mauris.sapien.cursus@malesuadaaugueut.ca','290-5329 At, Avenue','3418 RT','Baltasound','1969-12-31 16:00:00',68),
+(18,'Hayden','Abbott','Fusce','1-8716-875524','1-15-53646968','dictum.ultricies@arcuVestibulumut.co.uk','Ap #514-8014 Arcu Avenue','3109 IM','Mathura','1969-12-31 16:00:00',45),
+(19,'Lael','Jackson','Nunc','1-3622-915145','1-57-86592251','et.rutrum@sapienNuncpulvinar.com','178-9707 Tellus. St.','7010 WP','Fabro','1969-12-31 16:00:00',31),
+(20,'Silas','Nieves','blandit','1-5253-172665','1-28-83719192','Proin.dolor@sedleo.co.uk','Ap #788-4846 Dignissim Avenue','4195 QQ','Karimnagar','1969-12-31 16:00:00',36);
+SET IDENTITY_INSERT [dbo].[Medewerker] OFF
